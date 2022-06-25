8c0b709155e51fd107174c7be31fcc8dd037cee8
Abhijit Chikane <abhijit.chikane@varian.com>
2022-06-25 16:51:08 +0530

    added unit testing and e2e

--- src/app/calculator/calculator.component.spec.ts
+++ src/app/calculator/calculator.component.spec.ts
@@ -1,5 +1,5 @@
 import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
-import { ReactiveFormsModule } from '@angular/forms';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 
 import { CalculatorComponent } from './calculator.component';
 
@@ -7,21 +7,44 @@ describe('CalculatorComponent', () => {
   let component: CalculatorComponent;
   let fixture: ComponentFixture<CalculatorComponent>;
 
-  beforeEach(waitForAsync(() => {
+  beforeEach(() => {
     TestBed.configureTestingModule({
-      declarations: [ CalculatorComponent ],
+      declarations: [CalculatorComponent],
       imports: [ReactiveFormsModule],
     })
-    .compileComponents();
-  }));
+      .compileComponents();
+  });
 
   beforeEach(() => {
     fixture = TestBed.createComponent(CalculatorComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
+    component.calculatorForm = new FormGroup({
+      number1: new FormControl(6),
+      number2: new FormControl(2),
+    })
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should add the two numbers', () => {
+    component.add();
+    expect(component.result).toEqual(8);
+  })
+
+  it('should substract the second number from the first number', () => {
+    component.sub();
+    expect(component.result).toEqual(4);
+  })
+  it('should multiply the two numbers', () => {
+    component.mul();
+    expect(component.result).toEqual(12);
+  })
+
+  it('should devide the first number with the second number', () => {
+    component.div();
+    expect(component.result).toEqual(3);
+  })
 });
